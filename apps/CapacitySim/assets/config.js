cb9f979366cec4bee01fdfcaed6b9ea68ae4321a
Vladimir Acuña <vladimir.acuna.dev@gmail.com>
2026-01-19 17:20:50 -0300

    fix: implement cache busting for CSS, JS and JSON

--- apps/CapacitySim/assets/config.js
+++ apps/CapacitySim/assets/config.js
@@ -1,6 +1,6 @@
 window.BASELINES = null;
-window.__baselinesReady = (async function(){
-  const resp = await fetch('./data/baselines.json');
+window.__baselinesReady = (async function () {
+  const resp = await fetch('./data/baselines.json?t=' + Date.now());
   window.BASELINES = await resp.json();
   return window.BASELINES;
 })();
